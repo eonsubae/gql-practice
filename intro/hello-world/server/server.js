410c396bc3075ebb6a59b2da7c4097b1b4510f1e
eonsubae <eonsubae@naver.com>
2020-05-05 16:33:59 +0900

    Introduction of GraphQL

---
+++ intro/hello-world/server/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  schema {
+    query: Query
+  }
+
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: (_) => 'Hello GraphQL World!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen({ port: 9000 }).then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
