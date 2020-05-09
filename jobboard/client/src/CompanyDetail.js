742fbf1a873a4db2058cc4a049c309562d5ae9f8
eonsubae <eonsubae@naver.com>
2020-05-10 08:23:06 +0900

    Add error handling feature

--- jobboard/client/src/CompanyDetail.js
+++ jobboard/client/src/CompanyDetail.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import { companies } from './fake-data';
+import { JobList } from './JobList';
+import { loadCompany } from './request';
 
 export class CompanyDetail extends Component {
-  constructor(props) {
-    super(props);
-    const {companyId} = this.props.match.params;
-    this.state = {company: companies.find((company) => company.id === companyId)};
+  state = {
+    company: {},
+  };
+
+  async componentDidMount() {
+    const { companyId } = this.props.match.params;
+    const company = await loadCompany(companyId);
+    this.setState({ company });
   }
 
   render() {
-    const {company} = this.state;
+    const { company } = this.state;
+    if (!company) return null;
+
     return (
       <div>
         <h1 className="title">{company.name}</h1>
         <div className="box">{company.description}</div>
+        <h1 className="title is-5">Jobs at {company.name}</h1>
+        <JobList jobs={company.jobs} />
       </div>
     );
   }
--- jobboard/server/resolvers.js
+++ jobboard/server/resolvers.js
@@ -2,14 +2,21 @@ const db = require('./db');
 
 const Query = {
   jobs: () => db.jobs.list(),
-  job: (root, args) => db.jobs.get(args.id),
+  job: (root, { id }) => db.jobs.get(id),
+  company: (root, { id }) => db.companies.get(id),
 };
 
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
 module.exports = {
   Query,
   Job,
+  Company,
 };
