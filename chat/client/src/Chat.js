be3767a95abc0dcfe54f3e52b96bc9fe25a6df8b
eonsubae <eonsubae@naver.com>
2020-05-13 15:41:32 +0900

    Add useQuery hook

--- chat/client/src/Chat.js
+++ chat/client/src/Chat.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
+import { useQuery } from '@apollo/react-hooks';
+import React from 'react';
+import { messagesQuery } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
-  const [messages, setMessages] = useState([]);
+  const { /* loading, error, */ data } = useQuery(
+    messagesQuery /* { options... like in client.query } */
+  ); // 이 코드가 아래 두 줄과 같은 역할
+  // const [result, setResult] = useState({ loading: true });
+  // client.query({ query : messagesQuery }).then(({ data }) => setResult({ loading : false, data }));
+  const messages = data ? data.messages : [];
 
   const handleSend = async (text) => {
-    const message = { id: text, from: 'you', text };
-    setMessages(messages.concat(message));
+    // TODO
   };
+
+  // if (loading) return <Spinner />;
+  // if (error) return <div>Error!</div>;
 
   return (
     <section className="section">
