0eef8e4e8f74360f2f20534d7eda16dcd0011d2e
eonsubae <eonsubae@naver.com>
2020-05-13 16:10:55 +0900

    Add useSubscription hook

--- chat/client/src/Chat.js
+++ chat/client/src/Chat.js
@@ -1,24 +1,35 @@
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import React from 'react';
-import { messagesQuery, addMessageMutation } from './graphql/queries';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import React, { useState } from 'react';
+import {
+  messagesQuery,
+  addMessageMutation,
+  messageAddedSubscription,
+} from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
-  const { /* loading, error, */ data } = useQuery(
-    messagesQuery /* { options... like in client.query } */
-  ); // 이 코드가 아래 두 줄과 같은 역할
+  const [messages, setMessages] = useState([]);
+  useQuery(messagesQuery, {
+    onCompleted: ({ messages }) => setMessages(messages),
+  });
+  // const { /* loading, error, */ data } = useQuery(messagesQuery /* { options... like in client.query } */); // 이 코드가 아래 두 줄과 같은 역할
   // const [result, setResult] = useState({ loading: true });
   // client.query({ query : messagesQuery }).then(({ data }) => setResult({ loading : false, data }));
   const [addMessage /* { loading, error, data, called} */] = useMutation(
     addMessageMutation
   );
-  const messages = data ? data.messages : [];
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      setMessages(messages.concat(subscriptionData.data.messageAdded));
+    },
+  });
+  // const { data } = useSubscription(messageAddedSubscription);
+  // const messages = data ? [data.messageAdded] : [];
 
   const handleSend = async (text) => {
     // TODO
     const { data } = await addMessage({ variables: { input: { text } } });
-    console.log(data);
   };
 
   // if (loading) return <Spinner />;
